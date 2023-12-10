8002e0ac67282d6d3d7216d6912799ade37786f0
Arl3q1n <@Arl3q1n>
2023-12-10 17:27:48 +0200

    PS PAP2313

--- Homework-2/Exercise-1.cpp
+++ Homework-2/Exercise-1.cpp
@@ -1,56 +1,56 @@
-#include "iostream"
-#include "cmath"
-
-using namespace std;
-
-int main() {
-
-    int x;
-
-    cout << "[+] Introduce numarul: "; cin >> x;
-    cout << endl;
-
-    // Exercise 1
-    float result_1 = pow(x, 3) + pow(x, 2);
-    cout << "1. Resultat: " << result_1 << endl;
-
-    // Exercise 2
-    float result_2 = (1/x) + (1/pow(x, 2));
-    cout << "2. Resultat: " << result_2 << endl;
-
-    // Exercise 3
-    float result_3 = abs(x)/(pow(x, 2) + 1);
-    cout << "3. Resultat: " << result_3 << endl;
-
-    // Exercise 4
-    float result_4 = pow(x, 2) * log(x);
-    cout << "4. Resultat: " << result_4 << endl;
-
-    // Exercise 5
-    float result_5 = sin(x) + (1/2) * sin(2*x);
-    cout << "5. Resultat: " << result_5 << endl;
-
-    // Exercise 6
-    float result_6 = pow(x, 2) + (8/x);
-    cout << "6. Resultat: " << result_6 << endl;
-
-    // Exercise 7
-    float result_7 = pow(M_E, pow(-x, 2));
-    cout << "7. Resultat: " << result_7 << endl;
-
-    // Exercise 8
-    float result_8 = (pow(M_E, x) - pow(M_E, -x))/ 2;
-    cout << "8. Resultat: " << result_8 << endl;
-
-    // Exercise 9
-    float result_9 = (pow(M_E, x) - pow(M_E, -x))/(pow(M_E, x) + pow(M_E,  - x));
-    cout << "9. Resultat: " << result_9 << endl;
-
-    // Exercise 10
-    float result_10 = tan(x)/x;
-    cout << "10. Resultat: " << result_10 << endl;
-
-
-    return 0;
-
-}
+//#include "iostream"
+//#include "cmath"
+//
+//using namespace std;
+//
+//int main() {
+//
+//    int x;
+//
+//    cout << "[+] Introduce numarul: "; cin >> x;
+//    cout << endl;
+//
+//    // Exercise 1
+//    float result_1 = pow(x, 3) + pow(x, 2);
+//    cout << "1. Resultat: " << result_1 << endl;
+//
+//    // Exercise 2
+//    float result_2 = (1/x) + (1/pow(x, 2));
+//    cout << "2. Resultat: " << result_2 << endl;
+//
+//    // Exercise 3
+//    float result_3 = abs(x)/(pow(x, 2) + 1);
+//    cout << "3. Resultat: " << result_3 << endl;
+//
+//    // Exercise 4
+//    float result_4 = pow(x, 2) * log(x);
+//    cout << "4. Resultat: " << result_4 << endl;
+//
+//    // Exercise 5
+//    float result_5 = sin(x) + (1/2) * sin(2*x);
+//    cout << "5. Resultat: " << result_5 << endl;
+//
+//    // Exercise 6
+//    float result_6 = pow(x, 2) + (8/x);
+//    cout << "6. Resultat: " << result_6 << endl;
+//
+//    // Exercise 7
+//    float result_7 = pow(M_E, pow(-x, 2));
+//    cout << "7. Resultat: " << result_7 << endl;
+//
+//    // Exercise 8
+//    float result_8 = (pow(M_E, x) - pow(M_E, -x))/ 2;
+//    cout << "8. Resultat: " << result_8 << endl;
+//
+//    // Exercise 9
+//    float result_9 = (pow(M_E, x) - pow(M_E, -x))/(pow(M_E, x) + pow(M_E,  - x));
+//    cout << "9. Resultat: " << result_9 << endl;
+//
+//    // Exercise 10
+//    float result_10 = tan(x)/x;
+//    cout << "10. Resultat: " << result_10 << endl;
+//
+//
+//    return 0;
+//
+//}
--- Homework-3/Exercise_1.cpp
+++ Homework-3/Exercise_1.cpp
@@ -1,19 +1,19 @@
-#include "iostream"
-
-using namespace std;
-
-int main() {
-
-    int x, y;
-
-    cout << "[+] Introduce punctajul: "; cin >> x;
-    cout << "[+] Introduce punctajul: "; cin >> y;
-
-    if (x > y) {
-        cout << x << " puncte, " << y << " puncte";
-    } else {
-        cout << y << " puncte, " << x << " puncte";
-    }
-
-    return 0;
-}
+//#include "iostream"
+//
+//using namespace std;
+//
+//int main() {
+//
+//    int x, y;
+//
+//    cout << "[+] Introduce punctajul: "; cin >> x;
+//    cout << "[+] Introduce punctajul: "; cin >> y;
+//
+//    if (x > y) {
+//        cout << x << " puncte, " << y << " puncte";
+//    } else {
+//        cout << y << " puncte, " << x << " puncte";
+//    }
+//
+//    return 0;
+//}
